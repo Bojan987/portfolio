be790e3eeca39c86ffa6d12b2a9016b7984afa24
bojan987 <bojanmaj@hotmail.com>
2020-11-12 01:43:28 +0100

    added target

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -9,7 +9,7 @@ export const Footer = () => {
       <footer id="main-footer" className="p-5 bg-dark text-white">
         <Row>
           <Col md={6}>
-            <a href="src\image\Bojan Majmunovic-eng.pdf" className="btn btn-outline-light" download>
+            <a href="src\image\Bojan Majmunovic-eng.pdf" className="btn btn-outline-light" target="_blank" download>
             <i><FontAwesomeIcon icon={faCloud} className="fas"size="1x"/></i>
                 <span>  Download Resume</span>
             </a>
